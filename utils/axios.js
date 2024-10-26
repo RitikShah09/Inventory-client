b1d42dd666c863724e9899db30c6c47ca41e3a86
RitikShah09 <ritiksbs@gmail.com>
2024-10-26 11:47:02 +0530

    base url

--- utils/axios.js
+++ utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import getToken from "./getToken";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://inventory-server-26op.onrender.com/",
   withCredentials: true,
 });
 instance.interceptors.request.use(
